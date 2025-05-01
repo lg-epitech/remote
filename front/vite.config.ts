630fc25702e2a414778ed436ceade6f3637e41e0
lg-epitech <laurent.gonzalez@epitech.eu>
2025-05-01 05:15:10 +0200

    fix(vite): conf

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,6 +6,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    host: '0.0.0.0',   // listen on all interfaces
+    port: 80,         // bind 80
+    strictPort: true  // fail if 80 is taken
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
